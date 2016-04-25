326f71349c341c7ee37f54cb3a213f13e27cbef6
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-04-25 18:23:12 -0400

    Added lab 02

--- spring-boot-actuator/final/hello-spring-boot/src/main/java/lab/HelloSpringBootApplication.java
+++ spring-boot-actuator/final/hello-spring-boot/src/main/java/lab/HelloSpringBootApplication.java
@@ -1,5 +1,6 @@
 package lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HelloSpringBootApplication {
+	
+	@Autowired
+	GreetingService greetingService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
@@ -15,6 +19,6 @@ public class HelloSpringBootApplication {
 	
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello, World!";
+	    return String.format("%s World!", greetingService.getGreeting());
 	}
 }
